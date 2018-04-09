f419c6ba3240c9799c22fb146dfec8fb047b20e4
Andrey Petrov <andrey.petrov@shazow.net>
2018-04-09 13:44:57 -0400

    Random room

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,10 +3,19 @@
 </template>
 
 <script>
+function random_string(num, alphabet) {
+    const charset = alphabet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let r = '';
+    for (let i=num; i > 0; i--) {
+        r += charset[~~(Math.random() * charset.length)];
+    }
+    return r;
+}
+
 export default {
   name: 'Index',
   mounted() {
-    const roomId = 123
+    const roomId = random_string(8)
     this.$router.push({ name: 'room', params: { id: roomId }})
   },
 }
